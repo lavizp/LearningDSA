0bcd23ca1efc8e907bc706884d6f5b4c6128dee5
Laviz-pand <pandeylaviz@gmail.com>
2022-07-05 21:56:43 +0545

    easy

--- LeetCodeQwestions/Easy/Check if One String Swap Can Make Strings Equal.js
+++ LeetCodeQwestions/Easy/Check if One String Swap Can Make Strings Equal.js
@@ -1,18 +1,27 @@
-s1 = "abcd";
-s2 = "dcba";
+//https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/
 
-
-var areAlmostEqual = function(s1, s2) {
-    if( s1.length !== s2.length) return false;
-    let numofChanges = 0;
-    for(let i = 0; i < s1.length; i++)
-    {
-        c = s1[i];
-        if(!s2.includes(c)) return false;
-        if(c != s2[i]) numofChanges++;
-        if(numofChanges > 1) return false;
+var areAlmostEqual = function (s1, s2) {
+    let a = 0;
+      let diffs = [];
+  
+    if (s1 === s2) {
+      return true;
     }
-    return true;
-};
-
-console.log(areAlmostEqual(s1,s2));
+  
+    for (let i = 0; i < s1.length; i++) {
+      if (s1[i] !== s2[i]) {
+        a++;
+          diffs.push(i);
+      }
+      if (!s2.includes(s1[i])) {
+        return false;
+      }
+        
+    }
+      if(a == 2)
+      {
+          if(s1[diffs[0]] != s2[diffs[1]]) return false;
+      }
+  
+    return a === 2;
+  };
