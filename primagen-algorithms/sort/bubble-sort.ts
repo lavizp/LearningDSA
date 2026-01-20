56aa80a82167b59350020e7b73509314d9d5f3e1
lavizp <pandeylaviz@gmail.com>
2026-01-20 23:11:55 +0545

    feat: bubble sort

--- primagen-algorithms/sort/bubble-sort.ts
+++ primagen-algorithms/sort/bubble-sort.ts
@@ -1,3 +1,15 @@
 function BubbleSort(arr: Array<number>): Array<number> {
-  
+  for (let i = arr.length; i > 0 ; i-- ) {
+    for (let j = 0; j < i - 1; j++){
+      if (arr[j] > arr[j + 1]) {
+      const f = arr[j]
+      arr[j] = arr[j+1] 
+      arr[j+1]=f
+      }
+    }
+  console.log(arr)
+  }
+  return arr
 }
+
+console.log(BubbleSort([1,3,7,4,2,6,5]))
